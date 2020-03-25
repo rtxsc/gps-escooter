ec27e1aca455e6df32d4cfa4cb9baf02b2a976a6
zidz <zidios101@gmail.com>
2020-03-25 19:58:33 +0800

    simpletest

--- sim7000e-simpletest.py
+++ sim7000e-simpletest.py
@@ -2,13 +2,14 @@ from os import system
 import serial
 import subprocess
 from time import sleep
-from ISStreamer.Streamer import Streamer
+# from ISStreamer.Streamer import Streamer
 
-BUCKET_NAME = "movv-project"
-BUCKET_KEY = "SMMSQAMKS9Y9"
-ACCESS_KEY = "ist_PxI02ioOp4KEOeDtd2VfPyWpDdEZ82h6"
+# BUCKET_NAME = "movv-project"
+# BUCKET_KEY = "SMMSQAMKS9Y9"
+# ACCESS_KEY = "ist_PxI02ioOp4KEOeDtd2VfPyWpDdEZ82h6"
+
 SECONDS_BETWEEN_READS = 1
-INIT_DELAY = 5
+INIT_DELAY = 2
 READ_COUNT = 0
 STREAM_COUNT = 0
 DATA_POINT = 2 # GPS lat/lgt recorded before transmission
@@ -56,38 +57,38 @@ def closePPPD():
 
 # Check for a GPS fix
 def checkForFix():
-    print "checking for fix"
+    # print ("checking for fix")
     # Start the serial connection SIM7000E
-    # ser=serial.Serial('/dev/ttyS0', 9600, bytesize=serial.EIGHTBITS, parity=serial.PARITY_NONE, stopbits=serial.STOPBITS_ONE, timeout=1)
+    ser=serial.Serial('/dev/ttyS0', 9600, bytesize=serial.EIGHTBITS, parity=serial.PARITY_NONE, stopbits=serial.STOPBITS_ONE, timeout=1)
 
     # Start the serial connection SIM808
-    ser=serial.Serial('/dev/ttyS0', 115200, bytesize=serial.EIGHTBITS, parity=serial.PARITY_NONE, stopbits=serial.STOPBITS_ONE, timeout=1)
+    # ser=serial.Serial('/dev/ttyS0', 115200, bytesize=serial.EIGHTBITS, parity=serial.PARITY_NONE, stopbits=serial.STOPBITS_ONE, timeout=1)
 
     # Turn on the GPS
-    ser.write("AT+CGNSPWR=1\r")
-    ser.write("AT+CGNSPWR?\r")
+    ser.write(b"AT+CGNSPWR=1\r")
+    ser.write(b"AT+CGNSPWR?\r")
     while True:
         response = ser.readline()
-        if " 1" in response: # remove the whitespace before 1 for SIM7000E
-            print("GPS is ON!")
+        if b"1" in response: # remove the whitespace before 1 for SIM7000E
+            # print("GPS is ON!")
             break
     # Ask for the navigation info parsed from NMEA sentences
-    ser.write("AT+CGNSINF\r")
+    ser.write(b"AT+CGNSINF\r")
     print("Getting NMEA...")
     while True:
             response = ser.readline()
             # Check if a fix was found
-            if "+CGNSINF: 1,1," in response:
-                print "Fix found! OK!"
+            if b"+CGNSINF: 1,1," in response:
+                # print ("Fix found! OK!")
                 # print response
                 return True
             # If a fix wasn't found, wait and try again
-            if "+CGNSINF: 1,0," in response:
+            if b"+CGNSINF: 1,0," in response:
                 sleep(5)
-                ser.write("AT+CGNSINF\r")
-                print "Unable to find fix. still looking for fix..."
+                ser.write(b"AT+CGNSINF\r")
+                print ("Unable to find fix. still looking for fix...")
             else:
-                ser.write("AT+CGNSINF\r")
+                ser.write(b"AT+CGNSINF\r")
 
 # Read the GPS data for Latitude and Longitude
 def getCoord():
@@ -95,14 +96,14 @@ def getCoord():
     ser=serial.Serial('/dev/ttyS0', 9600, bytesize=serial.EIGHTBITS, parity=serial.PARITY_NONE, stopbits=serial.STOPBITS_ONE, timeout=1)
 
     # Start the serial connection SIM808
-    ser=serial.Serial('/dev/ttyS0', 115200, bytesize=serial.EIGHTBITS, parity=serial.PARITY_NONE, stopbits=serial.STOPBITS_ONE, timeout=1)
+    # ser=serial.Serial('/dev/ttyS0', 115200, bytesize=serial.EIGHTBITS, parity=serial.PARITY_NONE, stopbits=serial.STOPBITS_ONE, timeout=1)
 
-    ser.write("AT+CGNSINF\r")
+    ser.write(b"AT+CGNSINF\r")
     while True:
         response = ser.readline()
-        if "+CGNSINF: 1," in response:
+        if b"+CGNSINF: 1," in response:
             # Split the reading by commas and return the parts referencing lat and long
-            array = response.split(",")
+            array = response.split(b",")
             lat = array[3]
             # print lat
             lon = array[4]
@@ -114,8 +115,9 @@ def main_with_pppd():
     # Initialize the Initial State streamer
     # Start the program by opening the cellular connection and creating a bucket for our data
     if openPPPD():
-        print("\n\n\nOK ALRIGHT THEN! Everything looks good! Starting ISS streamer...")
-        streamer = Streamer(bucket_name=BUCKET_NAME, bucket_key=BUCKET_KEY, access_key=ACCESS_KEY, buffer_size=20)
+        # print("\n\n\nOK ALRIGHT THEN! Everything looks good! Starting ISS streamer...")
+        # streamer = Streamer(bucket_name=BUCKET_NAME, bucket_key=BUCKET_KEY, access_key=ACCESS_KEY, buffer_size=20)
+
         # Wait long enough for the request to complete
         for c in range(INIT_DELAY):
             print ("Starting in T-minus {} second".format(INIT_DELAY-c))
@@ -135,47 +137,51 @@ def main_with_pppd():
                         READ_COUNT+=1
                         latitude, longitude = getCoord()
                         coord = "lat:" + str(latitude) + "," + "lgt:" + str(longitude)
-                        print coord
+                        print (coord)
                         # Buffer the coordinates to be streamed
                         print("Saving read #{} into buffer.".format(READ_COUNT))
-                        streamer.log("Coordinates",coord)
+                        # streamer.log("Coordinates",coord)
                         sleep(SECONDS_BETWEEN_READS) # 1 second
                     # Turn the cellular connection on every 2 reads
                     if i == DATA_POINT-1:
                         sleep(1)
-                        print "opening connection"
+                        print ("opening connection")
                         if openPPPD():
                             STREAM_COUNT+=1
-                            print ("Streaming location to Initial State")
-#                             streamer.log("Read Count",str(READ_COUNT))
-                            streamer.log("Stream Count",str(STREAM_COUNT))
-                            # Flush the streaming buffer queue and send the data
-                            streamer.flush() # flush all the 4 readings to ISS
-                            print ("Streaming complete")
+                            # print ("Streaming location to Initial State")
+                            # streamer.log("Read Count",str(READ_COUNT))
+                            # streamer.log("Stream Count",str(STREAM_COUNT))
+                            # # Flush the streaming buffer queue and send the data
+                            # streamer.flush() # flush all the 4 readings to ISS
+                            # print ("Streaming complete")
 
 def main_without_pppd():
+    global READ_COUNT
     # Initialize the Initial State streamer
     # Start the program by opening the cellular connection and creating a bucket for our data
-    print("Starting streamer...")
-    streamer = Streamer(bucket_name=BUCKET_NAME, bucket_key=BUCKET_KEY, access_key=ACCESS_KEY, buffer_size=20)
+    for c in range(INIT_DELAY):
+        print ("Starting in T-minus {} second".format(INIT_DELAY-c))
+        sleep(1)
+    # streamer = Streamer(bucket_name=BUCKET_NAME, bucket_key=BUCKET_KEY, access_key=ACCESS_KEY, buffer_size=20)
     # Wait long enough for the request to complete
-    sleep(5)
     while True:
         # Make sure there's a GPS fix
         if checkForFix():
             # Get lat and long
             if getCoord():
+                READ_COUNT+=1
                 latitude, longitude = getCoord()
-                coord = "lat:" + str(latitude) + "," + "lgt:" + str(longitude)
-                print coord
+                coord = "lat:" + str(latitude) + "," + "lng:" + str(longitude)
+                print (coord)
+                print("Saving read #{} into buffer.\n".format(READ_COUNT))
                 # Buffer the coordinates to be streamed
-                streamer.log("SIM7000E Coordinates",coord)
+                # streamer.log("Coordinates",coord)
                 sleep(SECONDS_BETWEEN_READS)
-                print "streaming location to Initial State"
+                # print "streaming location to Initial State"
                 # Flush the streaming queue and send the data
-                streamer.flush()
-                print "streaming complete"
-                sleep(SECONDS_BETWEEN_READS)
+                # streamer.flush()
+                # print "streaming complete"
+
 
 if __name__ == "__main__":
     main_without_pppd()
